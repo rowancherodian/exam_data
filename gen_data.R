539e71955af2b942abbae021a646ee60c43f2972
rowancherodian <63108571+rowancherodian@users.noreply.github.com>
2022-02-07 15:31:15 +0000

    Update gen_data.R

--- gen_data.R
+++ gen_data.R
@@ -2,7 +2,7 @@ gen_data <- function(seedz){ # function to simulate data (do not change)
   if(is.na(seedz)==TRUE) return(print("change NA to YOUR Student No. (check student card)" ))
   n <- 100
   L <-matrix(0,n,n)
-  for (i in 1:n-1) L[i,(i+1)]<-1
+  diag(L[,-1]) <- 1
   set.seed(seedz)
   series <- solve((diag(n)-0.45*L),(1+0.35*seq(1:n)+rnorm(n,0,5)))   
   return(series)
