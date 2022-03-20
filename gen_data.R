539617689f511d5c1a5fa5474325e24f4b499591
rowancherodian <63108571+rowancherodian@users.noreply.github.com>
2022-03-20 19:01:00 +0000

    Update gen_data.R

--- gen_data.R
+++ gen_data.R
@@ -4,6 +4,6 @@ gen_data <- function(seedz){ # function to simulate data (do not change)
   L <-matrix(0,n,n)
   diag(L[,-1]) <- 1
   set.seed(seedz)
-  series <- solve((diag(n)-0.45*L),(1+0.35*seq(1:n)+rnorm(n,0,5)))   
+  series <- as.dataframe(solve((diag(n)-0.45*L),(1+0.35*seq(1:n)+rnorm(n,0,5))))   
   return(series)
 }
