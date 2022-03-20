74e94b209dd832bd0e8a3966019d149b0e65f928
rowancherodian <63108571+rowancherodian@users.noreply.github.com>
2022-03-20 19:04:32 +0000

    Create data5.R

---
+++ data5.R
@@ -0,0 +1,11 @@
+data5 <- function(seedz){ # function to simulate data (do not change) 
+  if(is.na(seedz)==TRUE) return(print("change NA to YOUR Student No. (check student card)" ))
+  n <- 100
+  L <-matrix(0,n,n)
+  diag(L[,-1]) <- 1
+  set.seed(seedz)
+  x <- rnorm(n,0,1)
+  y <- solve((diag(n)-0.45*L),(1+rnorm(n,0,5))) 
+  dataa <- as.data.frame(cbind(y,x))
+  return(series)
+}
